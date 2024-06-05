20823efaaa917c6a7081dab5b36a4eb9169ba258
Артур Аипов <aipov2@gmail.com>
2024-06-05 19:28:55 +0300

    latest mobile commit (fixed main client menu, now the create a new application button can only be processed after adding profile data and after adding users' cars)

--- Pages/ClientPages/MainMenuClientPage.xaml.cs
+++ Pages/ClientPages/MainMenuClientPage.xaml.cs
@@ -61,6 +61,21 @@ public partial class MainMenuClientPage : ContentPage
 
     private async void BIssue_Clicked(object sender, EventArgs e)
     {
+        var user = GlobalSettings.MainUser;
+        var drivers = await NetManager.Get<List<Driver>>("Drivers");
+        var driver = drivers.FirstOrDefault(x => x.UserId == GlobalSettings.MainUser.Id);
+        if (driver == null)
+        {
+            await Shell.Current.DisplayAlert("Ошибка", "Заполните свой профиль", "Ок");
+            return;
+        }
+        var cars = (await NetManager.Get<List<Car>>("Cars")).Where(x => x.DriverId == driver.Id).ToList();
+        if (cars.Count == 0)
+        {
+            await Shell.Current.DisplayAlert("Ошибка", "Добавьте машины", "Ок");
+            return;
+        }
+
         await Navigation.PushAsync(new ClientIssuePage());
     }
 }
